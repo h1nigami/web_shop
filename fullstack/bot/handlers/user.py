d578a10619b4cd1c8385b0e5b240934b0d57334b
h1nigami <timokhinivan1@gmail.com>
2023-10-31 13:40:26 +0300

    user handler

--- fullstack/bot/handlers/user.py
+++ fullstack/bot/handlers/user.py
@@ -1,4 +1,5 @@
 from loader import dp, bot, types
+from aiogram import F, Router
 from data import config, db
 
 from aiogram.fsm import FSMContext
@@ -6,3 +7,5 @@ from aiogram.fsm import FSMContext
 import logging
 
 logging.basicConfig(level=logging.INFO)
+
+router = Router()
