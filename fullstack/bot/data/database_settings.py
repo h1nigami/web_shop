30d907929646b597f4242332a8aff41c63467710
h1nigami <timokhinivan1@gmail.com>
2023-10-31 05:56:54 +0300

    create tables on db

--- fullstack/bot/data/database_settings.py
+++ fullstack/bot/data/database_settings.py
@@ -1,1 +1,14 @@
 import aiosqlite
+
+class DataBase:
+    def __init__(self, db_name):
+        self.db_name = db_name
+        
+    async def create_tables(self):
+        async with aiosqlite.connect(self.db_name) as db:
+            await db.execute("""CREATE TABLE IF NOT EXISTS users(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                username TEXT,)""")
+            await db.execute("""CREATE TABLE IF NOT EXISTS products(
+                name TEXT,
+                cost INTEGER,)""")
