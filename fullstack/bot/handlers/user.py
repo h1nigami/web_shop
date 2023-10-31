f6d54eee15672119db1a6ae3d30344d5b5b9db37
h1nigami <timokhinivan1@gmail.com>
2023-10-31 21:12:10 +0300

    first handler

--- fullstack/bot/handlers/user.py
+++ fullstack/bot/handlers/user.py
@@ -1,5 +1,6 @@
 from loader import dp, bot, types
 from aiogram import F, Router
+from aiogram.filters import Command
 from data import config, db
 
 from aiogram.fsm import FSMContext
@@ -9,3 +10,9 @@ import logging
 logging.basicConfig(level=logging.INFO)
 
 router = Router()
+
+@dp.message(Command(commands=['start']))
+async def startup(message:types.Message):
+    await db.create_user(tg_id=message.from_user.id, username=message.from_user.username)
+    await bot.send_photo(chat_id=message.from_user.id, photo=open(r'src/магазин.jpg'), caption='Добро пожаловать в магазин')
+    
