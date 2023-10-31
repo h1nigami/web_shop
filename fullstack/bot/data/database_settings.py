9ddc69ead1c94685966f7fc1d1932e64c02b2c8e
h1nigami <timokhinivan1@gmail.com>
2023-10-31 17:04:37 +0300

    db users

--- fullstack/bot/data/database_settings.py
+++ fullstack/bot/data/database_settings.py
@@ -7,11 +7,22 @@ class DataBase:
     async def create_tables(self):
         async with aiosqlite.connect(self.db_name) as db:
             await db.execute("""CREATE TABLE IF NOT EXISTS users(
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                id INTEGER PRIMARY KEY,
                 username TEXT,)""")
             await db.execute("""CREATE TABLE IF NOT EXISTS products(
                 name TEXT,
                 cost INTEGER,)""")
             await db.commit()
             
-db = DataBase('db.sqlite3')
+    async def create_user(self, tg_id, username):
+        async with aiosqlite.connect(self.db_name) as db:
+            await db.execute("INSERT INTO users VALUES (?, ?)", (tg_id, username))
+            await db.commit()
+    
+    async def get_user(self, username):
+        async with aiosqlite.connect(self.db_name) as db:
+            async with db.execute("SELECT * FROM users WHERE username = ?", (username,)) as cursor:
+                user = await cursor.fetchone()
+                return user
+            
+db = DataBase(r'fullstack\db.sqlite3')
