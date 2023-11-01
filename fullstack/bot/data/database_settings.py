6efa7f9452f4e90856d8ebdd726df4800a676d23
h1nigami <timokhinivan1@gmail.com>
2023-11-01 08:59:15 +0300

    commit

--- fullstack/bot/data/database_settings.py
+++ fullstack/bot/data/database_settings.py
@@ -3,7 +3,7 @@ import aiosqlite
 class DataBase:
     def __init__(self, db_name):
         self.db_name = db_name
-        
+    #создание таблиц    
     async def create_tables(self):
         async with aiosqlite.connect(self.db_name) as db:
             await db.execute("""CREATE TABLE IF NOT EXISTS users(
@@ -13,7 +13,8 @@ class DataBase:
                 name TEXT,
                 cost INTEGER)""")
             await db.commit()
-            
+    
+    #работа с юзерами        
     async def create_user(self, tg_id, username):
         try:
             async with aiosqlite.connect(self.db_name) as db:
@@ -27,5 +28,11 @@ class DataBase:
             async with db.execute("SELECT * FROM users WHERE username = ?", (username,)) as cursor:
                 user = await cursor.fetchone()
                 return user
+    #работа с ботами        
+    async def get_token(self, owner_id):
+        async with aiosqlite.connect(self.db_name) as db:
+            async with db.execute("SELECT token FROM backend_api_bots WHERE owner_id = ?", (owner_id,)) as cursor:
+                token = await cursor.fetchone()
+                return token
             
 db = DataBase(r'fullstack\db.sqlite3')
